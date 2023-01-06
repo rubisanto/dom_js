88bc1b4bc6e79be9f456c230c80fc4b099506de9
Antoine Gogce <antoinegogceapb@hotmail.fr>
2023-01-06 16:11:57 +0100

    add 07

---
+++ 07_idSelector.js
@@ -0,0 +1,6 @@
+function idSelector() {
+  //select the element input with the id "input"
+  let element = document.querySelector("#input");
+  //return the content of the element
+  return element.textContent;
+}
