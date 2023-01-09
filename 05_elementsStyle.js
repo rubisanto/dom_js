6366b08425cb5ad1273c92e92381e337079530f1
Antoine Gogce <antoinegogceapb@hotmail.fr>
2023-01-09 09:36:56 +0100

    modif 5

--- 05_elementsStyle.js
+++ 05_elementsStyle.js
@@ -2,9 +2,8 @@ const main = () => {
   let element = document.querySelectorAll("button");
   //   changer la couleur de tous les boutons
   element.forEach((button) => {
-    button.style.color = "red";
+    button.style.backgroundColor = "red";
   });
-  console.log(element);
   return element;
 };
 
