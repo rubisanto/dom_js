57380598eedca1adc1646e90b1575bda431759ff
Antoine Gogce <antoinegogceapb@hotmail.fr>
2023-01-09 15:55:16 +0100

    modif 16

--- 16_contactForm.js
+++ 16_contactForm.js
@@ -1,17 +1,26 @@
 // Sélectionne le formulaire
 let form = document.querySelector("form");
 
-// Sélectionne tous les champs de formulaire (input et button)
-let fields = form.querySelectorAll("input, button");
+// Sélectionne le bouton de soumission du formulaire
+let submitButton = form.querySelector("button[type='submit']");
 
-// Crée une balise p avec l'ID "result"
-let p = document.createElement("p");
-p.id = "result";
+// Ajoute un gestionnaire d'événement de clic au bouton
+submitButton.addEventListener("click", function (event) {
+  // Empêche le formulaire de soumettre les données
+  event.preventDefault();
 
-// Pour chaque champ de formulaire, ajoute le nom et la valeur à la balise p
-fields.forEach(function (field) {
-  p.textContent += `${field.name}: ${field.value}, `;
+  // Sélectionne tous les champs de formulaire (input et button)
+  let fields = form.querySelectorAll("input, button");
+
+  // Crée une balise p avec l'ID "result"
+  let p = document.createElement("p");
+  p.id = "result";
+
+  // Pour chaque champ de formulaire, ajoute le nom et la valeur à la balise p
+  fields.forEach(function (field) {
+    p.textContent += `${field.name}: ${field.value}, `;
+  });
+
+  // Ajoute la balise p au formulaire
+  form.appendChild(p);
 });
-
-// Ajoute la balise p au formulaire
-form.appendChild(p);
