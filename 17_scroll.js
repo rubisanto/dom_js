542232797cf37d64f41087c52ca4f54fed2e4580
Antoine Gogce <antoinegogceapb@hotmail.fr>
2023-01-10 09:41:50 +0100

    modif 17

--- 17_scroll.js
+++ 17_scroll.js
@@ -1,4 +1,11 @@
 // Crée un nouvel élément p avec la date actuelle
+let initialP = document.createElement("p");
+initialP.textContent = new Date().toLocaleString();
+
+// Ajout de l'élément p crée précédemment au body de la page
+document.body.appendChild(initialP);
+
+// Fonction pour créer un nouveau paragraphe avec la date et l'heure actuelle
 function createParagraph() {
   let p = document.createElement("p");
   p.textContent = new Date().toLocaleString();
@@ -7,8 +14,11 @@ function createParagraph() {
 
 // Ajoute un gestionnaire d'événement de défilement à la fenêtre
 window.addEventListener("scroll", function () {
-  // Si l'utilisateur a atteint le bas de la page, ajoute un nouvel élément p au DOM
-  if (window.scrollY + window.innerHeight >= document.body.offsetHeight) {
+  // Si l'utilisateur a défilé de plus de 50px depuis le dernier p ajouté
+  if (window.scrollY > 50) {
+    // Ajout d'un nouveau paragraphe avec la date et l'heure actuelle
     document.body.appendChild(createParagraph());
+    // Réinitialisation de la position de scrollY pour permettre l'ajout de nouveaux paragraphe
+    window.scrollY = 0;
   }
 });
