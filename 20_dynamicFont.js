ab367c8ccd503e0a0309094ce821c9643c89168f
Antoine Gogce <antoinegogceapb@hotmail.fr>
2023-01-10 10:00:19 +0100

    modif 20

--- 20_dynamicFont.js
+++ 20_dynamicFont.js
@@ -1,7 +1,12 @@
 // Sélectionne le formulaire
 let form = document.querySelector("form");
 
-let fontSizeField = document.querySelector("input[value='fontsize']");
+// créer le champ de formulaire de type nombre avec comme id "fontsize"
+let fontSizeField = document.createElement("input");
+fontSizeField.type = "number";
+fontSizeField.id = "fontsize";
+// l'ajouter au formulaire
+form.appendChild(fontSizeField);
 
 // Sélectionne les éléments possédant la classe "controlled_fontsize"
 let elements = document.querySelectorAll(".controlled_fontsize");
