dd375da04ceb4188a30dbe3f4a4b9622bb1643d1
Antoine Gogce <antoinegogceapb@hotmail.fr>
2023-01-12 11:48:50 +0100

    modif 26

--- 26_myPromiseTimeoutFailed_then.js
+++ 26_myPromiseTimeoutFailed_then.js
@@ -3,30 +3,29 @@ import { sleepThrow } from "./25_myPromiseTimeoutFailed.js";
 let success = document.querySelector("#success");
 let fail = document.querySelector("#fail");
 
-// add event listener to button
-success.addEventListener("click", function () {
-  // with then and catch add a p element to the DOM
-  sleepThrow(2000)
-    .then(function () {
-      let p = document.createElement("p");
-      p.textContent = "hello world";
-      document.body.appendChild(p);
-    })
-    .catch(function (error) {
-      console.log(error);
-    });
+successButton.addEventListener("click", async function () {
+  try {
+    await sleep(3000);
+    let success = document.createElement("p");
+    success.classList.add("success");
+    success.innerHTML = "Request succeeded";
+    document.body.appendChild(success);
+  } catch (error) {
+    console.error(error);
+  }
 });
 
-// add event listener to button
-fail.addEventListener("click", function () {
-  // with then and catch add a p element to the DOM
-  sleepThrow(1000)
-    .then(function () {
-      let p = document.createElement("p");
-      p.textContent = "hello world";
-      document.body.appendChild(p);
-    })
-    .catch(function (error) {
-      console.log(error);
-    });
+failButton.addEventListener("click", async function () {
+  try {
+    await sleepThrow(3000);
+    let success = document.createElement("p");
+    success.classList.add("success");
+    success.innerHTML = "Request succeeded";
+    document.body.appendChild(success);
+  } catch (error) {
+    let fail = document.createElement("p");
+    fail.classList.add("error");
+    fail.innerHTML = "Request failed";
+    document.body.appendChild(fail);
+  }
 });
