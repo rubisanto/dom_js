0c55b114a850bb543c40a9f6d10e9a062f4c5636
Antoine Gogce <antoinegogceapb@hotmail.fr>
2023-01-12 11:41:34 +0100

    modif 25

--- 25_myPromiseTimeoutFailed.js
+++ 25_myPromiseTimeoutFailed.js
@@ -1,20 +1,13 @@
-let button = document.querySelector("button");
-
-// add event listener to button
-button.addEventListener("click", function () {
-  //   sleepThrow() function throws an error with newPromise((resolve, reject) => {reject("error")}) without then
-  //   or await
-  function sleepThrow(ms) {
-    return new Promise((resolve) => {
-      setTimeout(function () {
-        if (ms === 2000) {
-          resolve();
-        } else {
-          reject("error");
-        }
-      }, ms);
-    });
-  }
-});
+const sleepThrow = (ms, boolean) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (boolean) {
+        resolve("success");
+      } else {
+        reject("failed");
+      }
+    }, ms);
+  });
+};
 
 module.exports = sleepThrow;
