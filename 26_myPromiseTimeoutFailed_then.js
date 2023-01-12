b85a7259ff9c0f80d5ccf20b8cc33e4553ebaa3f
Antoine Gogce <antoinegogceapb@hotmail.fr>
2023-01-12 16:57:00 +0100

    modif 26

--- 26_myPromiseTimeoutFailed_then.js
+++ 26_myPromiseTimeoutFailed_then.js
@@ -17,15 +17,12 @@ successButton.addEventListener("click", (e) => {
 failButton.addEventListener("click", () => {
   sleepThrow(3000, false)
     .then(() => {
-      let success = document.createElement("p");
-      success.classList.add("success");
-      success.innerHTML = "Request succeeded";
-      document.body.appendChild(success);
-    })
-    .catch((error) => {
       let fail = document.createElement("p");
       fail.classList.add("error");
       fail.innerHTML = "Request failed";
       document.body.appendChild(fail);
+    })
+    .catch((error) => {
+      console.error(error);
     });
 });
