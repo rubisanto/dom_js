56f212c8785850fd8aa402955ecf97374700e937
Antoine Gogce <antoinegogceapb@hotmail.fr>
2023-01-13 09:15:26 +0100

    modif 26

--- 23_myPromiseTimeout_then.js
+++ 23_myPromiseTimeout_then.js
@@ -2,16 +2,16 @@ let button = document.querySelector("button");
 
 // create a function that returns a promise
 function sleep(ms) {
-  return new Promise(function (resolve) {
-    setTimeout(function () {
+  return new Promise((resolve) => {
+    setTimeout(() => {
       resolve();
     }, ms);
   });
 }
 
-button.addEventListener("click", function () {
+button.addEventListener("click", () => {
   // sleep fuction to create a p element in the DOM
-  sleep(2000).then(function () {
+  sleep(2000).then(() => {
     let p = document.createElement("p");
     p.textContent = "hello world";
     document.body.appendChild(p);
