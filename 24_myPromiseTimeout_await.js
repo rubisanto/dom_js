fbe5dd9e57104b09c15d80af7d30b93cbcbe5b4c
Antoine Gogce <antoinegogceapb@hotmail.fr>
2023-01-13 09:17:49 +0100

    modif 24

--- 24_myPromiseTimeout_await.js
+++ 24_myPromiseTimeout_await.js
@@ -1,14 +1,19 @@
 let button = document.querySelector("button");
 
+// create a function that returns a promise
+function sleep(ms) {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve();
+    }, ms);
+  });
+}
+
 button.addEventListener("click", async () => {
   await sleep(2000); // attend 2 secondes
   const p = document.createElement("p");
   p.textContent = "Element ajouté après 2 secondes";
   document.body.appendChild(p);
 });
-
-async function sleep(ms) {
-  return new Promise((resolve) => setTimeout(resolve, ms));
-}
 
 module.exports = sleep;
